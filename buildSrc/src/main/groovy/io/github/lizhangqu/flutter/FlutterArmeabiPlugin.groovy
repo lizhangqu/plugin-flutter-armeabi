21e786845c468ab37aa431e2bcb931adf6f66cbc
李樟取 <lizhangqu@weidian.com>
2019-02-28 21:03:42 +0800

    * flutter armeabi plugin

--- buildSrc/src/main/groovy/io/github/lizhangqu/flutter/FlutterArmeabiPlugin.groovy
+++ buildSrc/src/main/groovy/io/github/lizhangqu/flutter/FlutterArmeabiPlugin.groovy
@@ -1,7 +1,17 @@
 package io.github.lizhangqu.flutter
 
-import org.gradle.api.Plugin
+import java.nio.file.Path
+import java.nio.file.Paths
+
+import com.android.builder.model.AndroidProject
+import org.apache.tools.ant.taskdefs.condition.Os
+import org.gradle.api.GradleException
 import org.gradle.api.Project
+import org.gradle.api.Plugin
+import org.gradle.api.Task
+import org.gradle.api.tasks.bundling.Jar
+
+import java.util.zip.ZipFile
 
 /**
  * @author lizhangqu
@@ -9,9 +19,246 @@ import org.gradle.api.Project
  * @since 2019-02-28 20:08
  */
 class FlutterArmeabiPlugin implements Plugin<Project> {
+    private File flutterRoot
+    private File flutterExecutable
+    private Properties localProperties
+
+    private File flutterJar
+    private File debugFlutterJar
+    private File profileFlutterJar
+    private File releaseFlutterJar
+    private File dynamicProfileFlutterJar
+    private File dynamicReleaseFlutterJar
+
+    // The name prefix for flutter builds.  This is used to identify gradle tasks
+    // where we expect the flutter tool to provide any error output, and skip the
+    // standard Gradle error output in the FlutterEventLogger. If you change this,
+    // be sure to change any instances of this string in symbols in the code below
+    // to match.
+    static final String flutterBuildPrefix = "flutterBuild"
+
+    private static Properties readPropertiesIfExist(File propertiesFile) {
+        Properties result = new Properties()
+        if (propertiesFile.exists()) {
+            propertiesFile.withReader('UTF-8') { reader -> result.load(reader) }
+        }
+        return result
+    }
+
+    private String resolveProperty(Project project, String name, String defaultValue) {
+        if (localProperties == null) {
+            localProperties = readPropertiesIfExist(new File(project.projectDir.parentFile, "local.properties"))
+        }
+        String result
+        if (project.hasProperty(name)) {
+            result = project.property(name)
+        }
+        if (result == null) {
+            result = localProperties.getProperty(name)
+        }
+        if (result == null) {
+            result = defaultValue
+        }
+        return result
+    }
 
     @Override
     void apply(Project project) {
+        String flutterRootPath = resolveProperty(project, "flutter.sdk", System.env.FLUTTER_ROOT)
+        if (flutterRootPath == null) {
+            project.logger.error("Flutter SDK not found. Define location with flutter.sdk in the local.properties file or with a FLUTTER_ROOT environment variable.")
+            return
+        }
+        flutterRoot = project.file(flutterRootPath)
+        if (!flutterRoot.isDirectory()) {
+            throw new GradleException("flutter.sdk must point to the Flutter SDK directory")
+        }
 
+        String flutterExecutableName = Os.isFamily(Os.FAMILY_WINDOWS) ? "flutter.bat" : "flutter"
+        flutterExecutable = Paths.get(flutterRoot.absolutePath, "bin", flutterExecutableName).toFile();
+
+        if (project.hasProperty('localEngineOut')) {
+            String engineOutPath = project.property('localEngineOut')
+            File engineOut = project.file(engineOutPath)
+            if (!engineOut.isDirectory()) {
+                throw new GradleException('localEngineOut must point to a local engine build')
+            }
+            flutterJar = Paths.get(engineOut.absolutePath, "flutter.jar").toFile()
+            if (!flutterJar.isFile()) {
+                throw new GradleException('Local engine build does not contain flutter.jar')
+            }
+            project.dependencies {
+                addFlutterArmeabiJarApiDependencyForLocalEngine(project, flutterJar)
+            }
+        } else {
+            Path baseEnginePath = Paths.get(flutterRoot.absolutePath, "bin", "cache", "artifacts", "engine")
+            String targetArch = 'arm'
+            if (project.hasProperty('target-platform') &&
+                    project.property('target-platform') == 'android-arm64') {
+                targetArch = 'arm64'
+            }
+            debugFlutterJar = baseEnginePath.resolve("android-${targetArch}").resolve("flutter.jar").toFile()
+            profileFlutterJar = baseEnginePath.resolve("android-${targetArch}-profile").resolve("flutter.jar").toFile()
+            releaseFlutterJar = baseEnginePath.resolve("android-${targetArch}-release").resolve("flutter.jar").toFile()
+            dynamicProfileFlutterJar = baseEnginePath.resolve("android-${targetArch}-dynamic-profile").resolve("flutter.jar").toFile()
+            dynamicReleaseFlutterJar = baseEnginePath.resolve("android-${targetArch}-dynamic-release").resolve("flutter.jar").toFile()
+            project.android.buildTypes.each {
+                addFlutterArmeabiJarApiDependency(project, it, targetArch)
+            }
+            project.android.buildTypes.whenObjectAdded {
+                addFlutterArmeabiJarApiDependency(project, it, targetArch)
+            }
+        }
+    }
+
+    /**
+     * create armeabi task
+     */
+    private Task createArmeabiTask(Project project, String taskName, File flutterJar, File flutterArmV7Jar) {
+        if (flutterJar == null || !flutterJar.exists()) {
+            return null
+        }
+
+        //如果已经包含armeabi，则不创建
+        ZipFile zipFile = null
+        try {
+            boolean alreadyContainsArmeabi = ((zipFile = new ZipFile(flutterJar)).getEntry("lib/armeabi/libflutter.so") != null)
+            if (alreadyContainsArmeabi) {
+                return null
+            }
+        } finally {
+            if (zipFile != null) {
+                zipFile.close()
+            }
+        }
+
+        def flutterArmeabiJarTask = project.tasks.create(taskName, Jar) {
+            destinationDir flutterArmV7Jar.parentFile
+            archiveName flutterArmV7Jar.name
+            from(project.zipTree(flutterJar).matching {
+                //仅包含armeabi-v7a的so
+                include "lib/armeabi-v7a/libflutter.so"
+            }) {
+                eachFile { def fcp ->
+                    if (fcp.relativePath.pathString.contains("lib/armeabi-v7a/libflutter.so")) {
+                        //重命名路径到armeabi目录
+                        fcp.path = "lib/armeabi/libflutter.so"
+                    } else {
+                        //非so排除
+                        fcp.exclude()
+                    }
+                }
+            }
+        }
+        return flutterArmeabiJarTask
+    }
+
+    /**
+     * add armeabi so for local engine
+     */
+    private void addFlutterArmeabiJarApiDependencyForLocalEngine(Project project, File localFlutterJar) {
+        if (this.flutterJar == null) {
+            return
+        }
+        File flutterJar = localFlutterJar
+        File flutterArmV7Jar = project.file("${project.buildDir}/${com.android.builder.model.AndroidProject.FD_INTERMEDIATES}/flutter/armeabi/flutter-armeabi.jar")
+
+        def flutterArmeabiJarTask = createArmeabiTask(project, "${flutterBuildPrefix}ArmeabiJar", flutterJar, flutterArmV7Jar)
+
+        if (flutterArmeabiJarTask == null) {
+            return
+        }
+
+        Task preBuildTask = project.tasks.findByName("preBuild")
+        if (preBuildTask) {
+            preBuildTask.dependsOn flutterArmeabiJarTask
+        }
+
+        project.dependencies {
+            String configuration;
+            if (project.getConfigurations().findByName("api")) {
+                configuration = "api";
+            } else {
+                configuration = "compile";
+            }
+            add(configuration, project.files {
+                flutterArmeabiJarTask
+            })
+        }
+    }
+
+    /**
+     * add armeabi so for prebuilt engine
+     */
+    private void addFlutterArmeabiJarApiDependency(Project project, def buildType, String targetArch) {
+        def flutterArmeabiJarTask = null
+        if (targetArch == 'arm') {
+            File flutterJar = flutterJarFor(buildModeFor(buildType))
+            File flutterArmV7Jar = project.file("${project.buildDir}/${AndroidProject.FD_INTERMEDIATES}/flutter/armeabi/${buildType.name}/flutter-armeabi.jar")
+            flutterArmeabiJarTask = createArmeabiTask(project, "${flutterBuildPrefix}${buildType.name.capitalize()}ArmeabiJar", flutterJar, flutterArmV7Jar)
+        }
+        if (flutterArmeabiJarTask == null) {
+            return
+        }
+
+        Task javacTask = project.tasks.findByName("compile${buildType.name.capitalize()}JavaWithJavac")
+        if (javacTask) {
+            javacTask.dependsOn flutterArmeabiJarTask
+        }
+        Task kotlinTask = project.tasks.findByName("compile${buildType.name.capitalize()}Kotlin")
+        if (kotlinTask) {
+            kotlinTask.dependsOn flutterArmeabiJarTask
+        }
+
+        project.dependencies {
+            String configuration;
+            if (project.getConfigurations().findByName("api")) {
+                configuration = buildType.name + "Api";
+            } else {
+                configuration = buildType.name + "Compile";
+            }
+            add(configuration, project.files {
+                flutterArmeabiJarTask
+            })
+        }
+    }
+
+    /**
+     * Returns a Flutter build mode suitable for the specified Android buildType.
+     *
+     * Note: The BuildType DSL type is not public, and is therefore omitted from the signature.
+     *
+     * @return "debug", "profile", "dynamicProfile", "dynamicRelease", or "release" (fall-back).
+     */
+    private static String buildModeFor(buildType) {
+        if (buildType.name == "profile") {
+            return "profile"
+        } else if (buildType.name == "dynamicProfile") {
+            return "dynamicProfile"
+        } else if (buildType.name == "dynamicRelease") {
+            return "dynamicRelease"
+        } else if (buildType.debuggable) {
+            return "debug"
+        }
+        return "release"
+    }
+
+    /**
+     * Returns a Flutter Jar file path suitable for the specified Android buildMode.
+     */
+    private File flutterJarFor(buildMode) {
+        if (flutterJar != null) {
+            return flutterJar
+        }
+        if (buildMode == "profile") {
+            return profileFlutterJar
+        } else if (buildMode == "dynamicProfile") {
+            return dynamicProfileFlutterJar
+        } else if (buildMode == "dynamicRelease") {
+            return dynamicReleaseFlutterJar
+        } else if (buildMode == "debug") {
+            return debugFlutterJar
+        }
+        return releaseFlutterJar
     }
 }
