c658c17b7a7cff1a4b0ad2e7fd81daee32ddd0bc
李樟取 <lizhangqu@weidian.com>
2019-02-28 21:03:19 +0800

    * init

---
+++ README.md
@@ -0,0 +1,17 @@
+### flutter armeabi gradle plugin
+
+
+```
+buildscript {
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        classpath "io.github.lizhangqu:plugin-flutter-armeabi:1.0.0"
+    }
+}
+
+apply plugin: 'flutter.armeabi'
+
+```
+
---
+++ buildSrc/src/main/groovy/io/github/lizhangqu/flutter/FlutterArmeabiPlugin.groovy
@@ -0,0 +1,17 @@
+package io.github.lizhangqu.flutter
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * @author lizhangqu
+ * @version V1.0
+ * @since 2019-02-28 20:08
+ */
+class FlutterArmeabiPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
